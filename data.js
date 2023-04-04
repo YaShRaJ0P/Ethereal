33addefe6cbc1ab89bcf3fd524a935e7713ddf6e
YaShRaJ0P <helloiamyashraj0405@gmail.com>
2023-04-04 19:58:57 +0530

    discussion forum added

---
+++ data.js
@@ -0,0 +1,43 @@
+var defaultThreads = [
+    {
+        id: 1,
+        title: "Why Sustainable development is important?",
+        author: "Aaron",
+        date: Date.now(),
+        content: "Why Sustainable development is important?",
+        comments: [
+            {
+                author: "Sushil",
+                date: Date.now(),
+                content: "because it is LED."
+            },
+            {
+                author: "Vicky",
+                date: Date.now(),
+                content: "Because I like LED."
+            }
+        ]
+    },
+    {
+        id: 2,
+        title: "Why should we follow sustainable living as it costs more money?",
+        author: "Yash",
+        date: Date.now(),
+        content: "Why should we follow sustainable living as it costs more money?",
+        comments: [
+            {
+                author: "Harsha",
+                date: Date.now(),
+                content: "Sustainable living can help reduce environmental degradation and pollution, conserve natural resources, and promote social and economic well-being."
+            },
+            {
+                author: "Sushil",
+                date: Date.now(),
+                content: "It improves your health. As for example cycling increases health and saves enviornment."
+            }
+        ]
+    }
+]
+
+var threads = defaultThreads
+    localStorage.setItem('threads', JSON.stringify(defaultThreads));
